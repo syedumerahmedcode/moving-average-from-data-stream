dfba7bd6c55605ac49b54e883bd2a549f8789ad5
umer <syedumerahmedcode@gmail.com>
2026-02-27 09:50:40 +0500

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * You need to implement a class that calculates the moving average of a stream of integers within a sliding window of a fixed size.
+        
+        The MovingAverage class should support two operations:
+        
+        Constructor MovingAverage(int size): Initializes the object with a window size. This window will hold at most size recent values from the stream.
+        
+        Method next(int val): Adds a new integer val to the stream and returns the average of the most recent values in the window. If the total number of values seen so far is less than the window size, it returns the average of all values seen so far.
+        
+        For example, if the window size is 3:
+        
+        After adding the first value, the average is just that value
+        After adding the second value, the average is of those two values
+        After adding the third value, the average is of all three values
+        After adding the fourth value, the window slides - it drops the first value and keeps only the most recent three values, then calculates their average
+         */
+        MovingAverage ma1 = new MovingAverage(3);
+        System.out.println(ma1.next(1)); // Expected output: 1.0
+        System.out.println(ma1.next(10)); // Expected output: 5.5
+        System.out.println(ma1.next(3)); // Expected output: 4.67
+        System.out.println(ma1.next(5)); // Expected output: 6.0
     }
 }
--- src/MovingAverage.java
+++ src/MovingAverage.java
@@ -1,3 +1,22 @@
-public class MovingAverage  {
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MovingAverage {
+
+    int size, windowSum = 0, count = 0;
+    Deque<Integer> queue = new ArrayDeque<>();
+
+    public MovingAverage(int size) {
+        this.size = size;
+    }
+
+    public double next(int val) {
+        ++count;
+        // calculate the new sum by shifting the window
+        queue.add(val);
+        int tail = count > size ? (int) queue.poll() : 0;
+        windowSum = windowSum - tail + val;
+        return windowSum * 1.0 / Math.min(size, count);
+    }
 
 }
